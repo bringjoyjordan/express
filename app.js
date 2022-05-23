6ae53a15f1d4a369d4ad160faf96c092a4ccad37
Daniel Karuna <kdsn1p3r@gmail.com>
2022-05-23 10:12:46 +0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var helmet = require('helmet');
 var app = express();
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://bringjoyjordan:w2Dwimzc1UpZzEvJ@cluster0.l39uw.mongodb.net/?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
